362cdbc9975a121566d26eb16f12a67470339403
riccardo lorenzon <riccardo.lorenzon@gmail.com>
2014-12-08 18:08:33 +0100

    hooked up views

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import patterns, include, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name = 'main')
+    url(r'^login/$', views.sign_in, name="sign-in"),
+    url(r'^$', views.index, name = 'main'),
+    url(r'^logout/$', views.logout_view, name = 'logout'),
+    url(r'^new_topic/$', views.new_topic, name='new-topic')
 )
--- messageboard/views.py
+++ messageboard/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from models import Topic
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def check_user_auth(user):
@@ -12,3 +13,29 @@ def index(request):
     topics = Topic.objects.all()
     ctx = {'topics': topics, 'user_login': check_user_auth(request.user)}
     return render(request, "topic-list.html", ctx)
+
+def logout_view(request):
+    logout(request)
+    return redirect("main")
+
+def sign_in(request):
+    username= request.POST['username']
+    password = request.POST['password']
+    user=authenticate(username=username, password = password)
+    if user is not None:
+        login(request, user)
+    return redirect('main')
+
+def new_topic(request):
+    topic_name=request.POST['topic_name']
+    newTopic = Topic()
+    newTopic.title = topic_name
+    newTopic.save()
+    return redirect('main')
+
+def topic_detail(request, topic_id=None):
+    topic = Topic.objects.get(id=topic_id)
+    posts = topic.post_set.all()
+    ctx = {'topic': topic, 'user_login': check_user_auth(request.user),
+           'posts': posts}
+    return render(request, 'topic-detail.html', ctx)
